de85ba4d69f128fbdcefe4addc71d2485bcc9e57
Tomasz Bury <tomasz.bury@onet.eu>
2024-03-26 13:42:22 +0100

    add file adding validation

--- webocr-frontend/src/components/Dropzone.jsx
+++ webocr-frontend/src/components/Dropzone.jsx
@@ -1,43 +1,90 @@
-import {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 import styled from "styled-components";
+import React, { Fragment, useState } from "react";
+import {filesize} from "filesize";
 
 const DropzoneHeader = styled.h2`
-    color: #00844E;
-    font-weight: bold;
-    font-family: 'Lexend Deca';
-`
+  color: #00844e;
+  font-weight: bold;
+  font-family: "Lexend Deca";
+`;
 
 const DropzoneHint = styled.p`
-    color: #374151;
-    margin-top: 12px;
-    font-size: 0.875rem;
-    line-height: 1.25rem;
-`
+  color: #374151;
+  margin-top: 12px;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+`;
+
+const AcceptedFiles = styled.div`
+  border: 1px solid #d1d5db;
+  padding: 2.5rem;
+  border-radius: 10px;
+  margin-top: 32px;
+`;
 
 function Dropzone() {
-    const onDrop = useCallback(acceptedFiles => {
-        console.log(acceptedFiles)
-    }, [])
-    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    const {
+        acceptedFiles,
+        fileRejections,
+        getRootProps,
+        getInputProps
+      } = useDropzone({
+        accept: {
+          'image/jpeg': [],
+          'image/png': []
+        },
+        maxFiles: 1,
+        maxSize: 10485760,
+      });
+    
+    
+      const fileRejectionItems = fileRejections.map(({ file, errors }) => (
+        <li key={file.path}>
+          {file.path} - {file.size} bytes
+          <ul>
+            {errors.map(e => (
+              <li key={e.code}>{e.message}</li>
+            ))}
+          </ul>
+        </li>
+      ));
 
-    return (
-        <form className="p-16 mt-10 border border-neutral-200 w-1/3 flex flex-col items-center justify-center">
+  return (
+    <>
+    { fileRejections.length > 0 && 
+        <h2 className="mt-4 text-red-700 font-bold">Wrzucone pliki są niepoprawne. Wrzuć pliki .jpg lub .png o rozmiarze maksymalnie 10 MB.</h2>
+    }
+      <form className="p-16 mt-4 border border-neutral-200 w-1/3 flex flex-col items-center justify-center">
+        
         <div {...getRootProps()} className="flex flex-col items-center">
-            <input {...getInputProps()} />
-            <img src="/upload-cloud.png" alt="" className="mb-4"/>
-            {
-                isDragActive ?
-                    <DropzoneHeader className="text-xl">Kliknij, by wrzucić zdjęcie</DropzoneHeader> :
-                    <div className="flex flex-row gap-2">
-                        <DropzoneHeader className="text-xl">Kliknij, by wrzucić zdjęcie</DropzoneHeader>
-                        <p className="text-xl">lub przeciągnij i upuść</p>
-                    </div>
-            }
-            <DropzoneHint>Dopuszczalne formaty JPG i PNG, maksymalnie 10 MB</DropzoneHint>
+          <input {...getInputProps()} />
+          <img src="/upload-cloud.png" alt="" className="mb-4" />
+          <div className="flex flex-row gap-2">
+              <DropzoneHeader className="text-xl">
+                Kliknij, by wrzucić zdjęcie
+              </DropzoneHeader>
+              <p className="text-xl">lub przeciągnij i upuść</p>
+            </div>
+          <DropzoneHint>
+            Dopuszczalne formaty JPG i PNG, maksymalnie 10 MB
+          </DropzoneHint>
         </div>
-        </form>
-    )
+      </form>
+      { acceptedFiles.length > 0 && acceptedFiles.map(file => (
+        <AcceptedFiles className="w-1/3" key={file.name}>
+        <div className="info-container flex flex-row items-center gap-4">
+            <img src="/image_icon.png" alt=""/>
+            <span className="flex flex-col">
+            <p className="font-bold text-lg">{file.name}</p>
+            <p className="text-sm">{filesize(file.size)}</p>
+        </span>
+        </div>
+        </AcceptedFiles>
+      ))}
+    </>
+  );
 }
 
 export default Dropzone;
--- webocr-frontend/src/pages/FirstNote.jsx
+++ webocr-frontend/src/pages/FirstNote.jsx
@@ -11,12 +11,7 @@ const FirstNoteHero = styled.div`
     justify-content: center;
 `
 
-const AcceptedFiles = styled.div`
-    border: 1px solid #D1D5DB;
-    padding: 2.5rem;
-    border-radius: 10px;
-    margin-top: 32px;
-`
+
 
 const NextButton = styled.button`
   background-color: #004423;
@@ -38,15 +33,7 @@ function FirstNote() {
                     <p className="max-w-3xl text-center mt-4 text-md">Przekonaj się, jak łatwe i przyjemne jest korzystanie z webOCR. Zacznijmy od utworzenia pierwszej notatki. Przeciągnij i upuść lub wybierz plik do wrzucenia, a system z użyciem AI rozpozna i zapisze tekst, który się tam znajduje.</p>
                 </FirstNoteHero>
                 <Dropzone></Dropzone>
-                <AcceptedFiles className="w-1/3">
-                    <div className="info-container flex flex-row items-center gap-4">
-                        <img src="/image_icon.png" alt=""/>
-                        <span className="flex flex-col">
-                        <p className="font-bold text-lg">notatka.jpg</p>
-                        <p className="text-sm">200 KB</p>
-                    </span>
-                    </div>
-                </AcceptedFiles>
+                
                 <NextButton className="mt-16 w-1/3"> Przejdź dalej &gt;</NextButton>
             </main>
 
