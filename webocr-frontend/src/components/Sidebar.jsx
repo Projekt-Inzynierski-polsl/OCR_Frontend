9fde399c98230efd9a4ea401d3deaa0b514f114d
Tomasz Bury <tomasz.bury@onet.eu>
2024-03-26 11:51:35 +0100

    create collapsible notes and folders

--- webocr-frontend/src/components/Sidebar.jsx
+++ webocr-frontend/src/components/Sidebar.jsx
@@ -1,155 +1,205 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import React, { Fragment, useState } from "react";
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from "@/components/ui/tooltip"
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const SidebarBody = styled.div`
-    background-color: #F3F4F6;
-    color: #374151;
-    height: 100vh;
-    padding-top: 64px;
-    font-family: 'Space Grotesk';
+  background-color: #f3f4f6;
+  color: #374151;
+  height: 100vh;
+  padding-top: 64px;
+  font-family: "Space Grotesk";
 `;
 
 const FolderHint = styled.p`
-    color: #9CA3AF; 
-`
+  color: #9ca3af;
+`;
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
-    return (
-        <>
-            <aside>
-                <SidebarBody>
-                    <h2 className="font-bold text-xl pl-16">Ostatnie dokumenty</h2>
-                    <div className="last-docs-container flex flex-col mt-2">
-                        <div
-                            className="last-doc flex flex-row gap-2 items-center hover:bg-slate-200 py-1.5 pl-12 pr-10 mx-4">
-                            <img src="/note.png" alt=""/>
-                            <a href='' className="text-ellipsis overflow-hidden whitespace-nowrap">testowy tekst który z
-                                pewnością jest bardzo długi</a>
-                        </div>
-                        <div
-                            className="last-doc flex flex-row gap-2 items-center hover:bg-slate-200 py-1.5 pl-12 pr-10 mx-4">
-                            <img src="/note.png" alt=""/>
-                            <a href='' className="text-ellipsis overflow-hidden whitespace-nowrap text-md">jeszcze
-                                dłuższy testowy tekst o niczym</a>
-                        </div>
-                        <div
-                            className="last-doc flex flex-row gap-2 items-center hover:bg-slate-200 py-1.5 pl-12 pr-10 mx-4">
-                            <img src="/note.png" alt=""/>
-                            <a href='' className="text-ellipsis overflow-hidden whitespace-nowrap">bardzo długi testowy
-                                tekst do sprawdzenia</a>
-                        </div>
+  const [userFolders, setUserFolders] = useState([
+    {
+      name: "Przyroda",
+      notesCount: 4,
+      notes: [
+        {
+          name: "Notatka 1",
+          content: "Treść notatki 1",
+          url: "/note",
+        },
+        {
+            name: "Notatka 2",
+            content: "Treść notatki 2",
+            url: "/note-2",
+        },
+        {
+            name: "Notatka 3",
+            content: "Treść notatki 3",
+            url: "/note-3",
+        },
+      ],
+    },
+    {
+      name: "Matematyka",
+      notesCount: 4,
+      notes: [
+        {
+          name: "Notatka 1",
+          content: "Treść notatki 1",
+          url: "/note-2",
+        },
+      ],
+    },
+    {
+      name: "Informatyka",
+      notesCount: 4,
+      notes: [
+        {
+          name: "Notatka 1",
+          content: "Treść notatki 1",
+          url: "/note-3",
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <>
+      <aside>
+        <SidebarBody>
+          <h2 className="font-bold text-xl pl-16">Ostatnie dokumenty</h2>
+          <div className="last-docs-container flex flex-col mt-2">
+            <div className="last-doc flex flex-row gap-2 items-center hover:bg-slate-200 py-1.5 pl-12 pr-10 mx-4">
+              <img src="/note.png" alt="" />
+              <a
+                href=""
+                className="text-ellipsis overflow-hidden whitespace-nowrap"
+              >
+                testowy tekst który z pewnością jest bardzo długi
+              </a>
+            </div>
+            <div className="last-doc flex flex-row gap-2 items-center hover:bg-slate-200 py-1.5 pl-12 pr-10 mx-4">
+              <img src="/note.png" alt="" />
+              <a
+                href=""
+                className="text-ellipsis overflow-hidden whitespace-nowrap text-md"
+              >
+                jeszcze dłuższy testowy tekst o niczym
+              </a>
+            </div>
+            <div className="last-doc flex flex-row gap-2 items-center hover:bg-slate-200 py-1.5 pl-12 pr-10 mx-4">
+              <img src="/note.png" alt="" />
+              <a
+                href=""
+                className="text-ellipsis overflow-hidden whitespace-nowrap"
+              >
+                bardzo długi testowy tekst do sprawdzenia
+              </a>
+            </div>
+          </div>
+          <div className="user-notes-container mt-24">
+            <div className="user-notes-header flex flex-row pl-16 items-center justify-between mr-2">
+              <h2 className="font-bold text-xl">Twoja przestrzeń</h2>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <button className="p-2 hover:bg-neutral-200 mr-1">
+                      <img src="/plus.svg" alt="" />
+                    </button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Stwórz nowy folder</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
+            <div className="user-notes">
+              {userFolders.map((folder) => (
+                <Fragment key={folder.name}>
+                  <Collapsible>
+                    <div className="folder pl-10 mt-2 flex flex-row hover:bg-slate-200 py-1.5 mx-4 items-center">
+                      <CollapsibleTrigger>
+                        <button
+                          className="p-2 hover:bg-neutral-300 rounded-md"
+                          onClick={(e) =>
+                            e.target.classList.toggle("rotate-90")
+                          }
+                        >
+                          <img src="/arr.svg" alt="" />
+                        </button>
+                      </CollapsibleTrigger>
+
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <button className="p-2 hover:bg-neutral-200 mr-1">
+                              <img src="/folder.png" alt="" />
+                            </button>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>Zmień ikonę folderu</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                      <p className="folder__title mr-4">{folder.name}</p>
+                      <FolderHint className="mr-24 text-sm">
+                        {folder.notesCount}
+                      </FolderHint>
                     </div>
-                    <div className="user-notes-container mt-24">
-                        <div className="user-notes-header flex flex-row pl-16 items-center justify-between mr-2">
-                            <h2 className="font-bold text-xl">Twoja przestrzeń</h2>
-                            <TooltipProvider>
-                                <Tooltip>
-                                    <TooltipTrigger>
-                                        <button className="p-2 hover:bg-neutral-200 mr-1">
-                                            <img src="/plus.svg" alt=""/>
-                                        </button>
-                                    </TooltipTrigger>
-                                    <TooltipContent>
-                                        <p>Stwórz nowy folder</p>
-                                    </TooltipContent>
-                                </Tooltip>
-                            </TooltipProvider>
-                        </div>
-                        <div className="user-notes">
-                            <div
-                                className="folder pl-10 mt-2 flex flex-row hover:bg-slate-200 py-1.5 mx-4 items-center">
-                                <button className="p-2 hover:bg-neutral-300 rounded-md">
-                                    <img src="/arr.svg" alt="" className="rotate-90"/>
-                                </button>
-                                <TooltipProvider>
-                                    <Tooltip>
-                                        <TooltipTrigger>
-                                            <button className="p-2 hover:bg-neutral-200 mr-1">
-                                                <img src="/folder.png" alt=""/>
-                                            </button>
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            <p>Zmień ikonę folderu</p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider>
-                                <p className="folder__title mr-4">Przyroda</p>
-                                <FolderHint className="mr-24 text-sm">4</FolderHint>
-
-                                <button className="p-1 hover:bg-neutral-300 rounded-md self-end">
-                                    <img src="/plus.svg" alt=""/>
-                                </button>
-
-                            </div>
-                            <div className="folder__notes pl-6">
+                    <CollapsibleContent>
+                      <div className="folder__notes pl-6">
+                        {folder.notes.map((note) => (
+                          <NavLink
+                            to={note.url}
+                            style={({ isActive }) => {
+                              return {
+                                backgroundColor: isActive
+                                  ? "#f3f4f6"
+                                  : "transparent",
+                                fontWeight: isActive ? "bold" : "normal",
+                              };
+                            }}
+                          >
+                            {({ isActive }) => {
+                              return isActive ? (
                                 <div
-                                    className="folder-doc flex flex-row gap-2 items-center hover:bg-slate-200 py-1.5 pl-12 pr-10 mx-4">
-                                    <img src="/note.png" alt=""/>
-                                    <a href='' className="text-ellipsis overflow-hidden whitespace-nowrap">testowy tekst
-                                        który z
-                                        pewnością jest bardzo długi</a>
+                                  className={`folder-doc bg-slate-200 flex flex-row gap-2 items-center py-1.5 pl-12 pr-10 mx-4`}
+                                >
+                                  <img src="/note.png" alt="" />
+                                  {note.name}
                                 </div>
+                              ) : (
                                 <div
-                                    className="folder-doc doc__selected flex flex-row gap-2 items-center bg-slate-200 font-bold py-1.5 pl-12 pr-10 mx-4">
-                                    <img src="/note.png" alt=""/>
-                                    <a href='' className="text-ellipsis overflow-hidden whitespace-nowrap">testowy tekst
-                                        który z
-                                        pewnością jest bardzo długi</a>
+                                  className={`folder-doc doc flex flex-row gap-2 items-center hover:bg-slate-200 py-1.5 pl-12 pr-10 mx-4`}
+                                >
+                                  <img src="/note.png" alt="" />
+                                  {note.name}
                                 </div>
-                            </div>
-                            <div
-                                className="folder pl-10 mt-2 flex flex-row hover:bg-slate-200 py-1.5 mx-4 items-center">
-                                <button className="p-2 hover:bg-neutral-300 rounded-md">
-                                    <img src="/arr.svg" alt=""/>
-                                </button>
-                                <TooltipProvider>
-                                    <Tooltip>
-                                        <TooltipTrigger>
-                                            <button className="p-2 hover:bg-neutral-200 mr-1">
-                                                <img src="/folder.png" alt=""/>
-                                            </button>
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            <p>Zmień ikonę folderu</p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider>
-                                <p className="folder__title mr-4">Matematyka</p>
-                                <FolderHint className="mr-24 text-sm">4</FolderHint>
-
-                            </div>
-                            <div
-                                className="folder pl-10 flex flex-row hover:bg-slate-200 py-1.5 mx-4 items-center">
-                                <button className="p-2 hover:bg-neutral-300 rounded-md">
-                                    <img src="/arr.svg" alt=""/>
-                                </button>
-                                <TooltipProvider>
-                                    <Tooltip>
-                                        <TooltipTrigger>
-                                            <button className="p-2 hover:bg-neutral-200 mr-1">
-                                                <img src="/folder.png" alt=""/>
-                                            </button>
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            <p>Zmień ikonę folderu</p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider>
-                                <p className="folder__title mr-4">Matematyka</p>
-                                <FolderHint className="mr-24 text-sm">4</FolderHint>
-
-                            </div>
-                        </div>
-                    </div>
-                </SidebarBody>
-            </aside>
-        </>
-    )
+                              );
+                            }}
+                          </NavLink>
+                        ))}
+                      </div>
+                    </CollapsibleContent>
+                  </Collapsible>
+                </Fragment>
+              ))}
+            </div>
+          </div>
+        </SidebarBody>
+      </aside>
+    </>
+  );
 }
 
 export default Sidebar;
